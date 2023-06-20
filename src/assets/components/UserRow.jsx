a8993acf393a5b37801b13ed55c559cf3310e2b9
Amnhed Lagunas <aalhe0.0@gmail.com>
2023-06-19 23:43:15 -0600

    main

--- src/assets/components/UserRow.jsx
+++ src/assets/components/UserRow.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
-export const UserRow = ( { id,username, email } ) => {
+export const UserRow = ( { handlerRemoveUser, id, username, email } ) => {
+  const onRemoveUser = (id) => {
+    handlerRemoveUser(id)
+  }
+
   return (
-    <tr>
-      <th scope="row"></th>
-      <td>{ id }</td>
-      <td>{ username }</td>
-      <td>{ email }</td>
-      <td>
-        <button type="button" className="btn btn-secondary sm">
-          Actualizar
-        </button>
-      </td>
-      <td>
-        <button type="button" className="btn btn-danger sm">
-          Eliminar
-        </button>
-      </td>
-    </tr>
+      <tr>
+        <th scope="row"></th>
+        <td>{ id }</td>
+        <td>{ username }</td>
+        <td>{ email }</td>
+        <td>
+          <button type="button" className="btn btn-secondary sm">
+            Actualizar
+          </button>
+        </td>
+        <td>
+          <button 
+            type="button" 
+            className="btn btn-danger sm"
+            onClick={ () => onRemoveUser(id) }
+          >
+            Eliminar
+          </button>
+        </td>
+      </tr>
   );
 }
