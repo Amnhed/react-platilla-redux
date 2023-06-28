35b44da19d1d0d7777453e99835049053bb9b487
Amnhed Lagunas <aalhe0.0@gmail.com>
2023-06-28 01:55:26 -0600

    Configuracion general de las rutas

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -5,23 +5,39 @@ import { loginReducer } from './auth/reducers/loginReducer';
 import { Navbar } from './components/layout/NavBar';
 import { UsersPage } from './pages/UsersPage';
 import { useAuth } from './auth/hooks/useAuth';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { UserRoutes } from './routes/userRoutes';
 
 
 
 export const MainApp = () => {
     const {login, handlerLogin, handlerLogout} = useAuth();
     return (
-        <>
+        <Routes>
             {
                 login.isAuth
                     ? (
                         <>
-                            <Navbar login={ login } handlerLogout={handlerLogout} />
-                            <UsersPage />
+                            <Route path='/*' 
+                                element={
+                                    <UserRoutes 
+                                        login={login}
+                                        handlerLogout={handlerLogout}
+                                    />
+                                    }
+                            />
                         </>
                     )
-                    : <LoginPage handlerLogin={handlerLogin} />
+                    : 
+                    <>
+                        <Route path='/login'
+                            element={
+                                <LoginPage handlerLogin={handlerLogin} />
+                            }
+                        />
+                        <Route path='/*' element={ <Navigate to="/login"/>}/>
+                    </>
             }
-        </>
+        </Routes>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { MainApp } from './MainApp'
 import './styles.css'
 import { UsersPage } from './pages/UsersPage'
 import { LoginPage } from './auth/pages/LoginPage'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainApp />
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
