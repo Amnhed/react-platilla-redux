071f8993507e4211870c53cc33e32a65166b4797
Amnhed Lagunas <aalhe0.0@gmail.com>
2023-07-04 00:14:30 -0600

    Implementacion del AuthProvider

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,13 +1,14 @@
 import { useReducer } from 'react';
 import { LoginPage } from './auth/pages/LoginPage';
-import { useAuth } from './auth/hooks/useAuth';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainRoutes } from './routes/MainRoutes';
+import { useContext } from 'react';
+import { AuthContext } from './auth/context/AuthContext';
 
 
 
 export const MainApp = () => {
-    const {login, handlerLogin, handlerLogout} = useAuth();
+    const {login} = useContext(AuthContext);
     return (
         <Routes>
             {
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MainApp } from './MainApp'
 import './styles.css'
-import { UsersPage } from './pages/UsersPage'
-import { LoginPage } from './auth/pages/LoginPage'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './auth/context/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MainApp />
+      <AuthProvider>
+        <MainApp />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
